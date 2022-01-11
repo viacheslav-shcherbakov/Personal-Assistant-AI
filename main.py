59a4a071f016ad88823e985fcae5c5e438d39c7b
josh-zenik <69052559+JOSH1059@users.noreply.github.com>
2022-01-11 12:37:18 -0500

    Random changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Personal Assistant v0.2
 Created by: Josh Vitvitsky
 10/20/2021 11:00 AM
 """
-import os 
+import os
 import time
 import json
 import random
@@ -18,6 +18,8 @@ from chatterbot.trainers import ListTrainer
 
 import custom_commands_handler
 #custom_commands_handler.boot()
+
+dirName=os.getcwd()
 
 #SETUP
 AI_NAME='Jarvis'
@@ -36,24 +38,39 @@ custom_trainer=ListTrainer(bot)
 
 trainer.train(
     #"chatterbot.corpus.english"
-    "chatterbot.corpus.english.greetings",
+    #"chatterbot.corpus.english.greetings",
     #"chatterbot.corpus.english.conversations"
 )
 
-formal_trainer=json.loads(open('custom_trainers/formal_train.json','r').read())
+print('Training Formal Speech Branch')
+formal_trainer=json.loads(open('{}/Personal-Assistant-AI/custom_trainers/formal_train.json'.format(dirName),'r').read())
 formal_trainer_list=[]
 for branch in formal_trainer:
     for line in formal_trainer[branch]:
         formal_trainer_list.append(line.replace('*N', AI_NAME))
         formal_trainer_list.append(formal_trainer[branch][line].replace('*N', AI_NAME))
 
-print(formal_trainer_list)
 custom_trainer.train(formal_trainer_list)
+
+
+print('Training Commands Branch')
+commands_trainer=json.loads(open('{}/Personal-Assistant-AI/commands/commands.json'.format(dirName),'r').read())
+commands_trainer_list=[]
+for command in commands_trainer['commands']:
+    print('Command Training: Now training: {} Modual \n\tDetails: {} \n\tCreated: {}'.format(commands_trainer['commands'][command]['info']['name'],commands_trainer['commands'][command]['info']['details'],commands_trainer['commands'][command]['info']['creation_date']))
+    for line in commands_trainer['commands'][command]['training']:
+        commands_trainer_list.append(line.replace('*N', AI_NAME))
+        commands_trainer_list.append(commands_trainer['commands'][command]['training'][line].replace('*N', AI_NAME))
+
+custom_trainer.train(formal_trainer_list)
+
 
 def main():
     in_convo=False
     in_convo_time=datetime.now()
     print('{} is online.'.format(AI_NAME))
+    #BOOT COMMAND HANDLER
+    c=command_handler()
     # The following loop will execute each time the user enters input
     while True:
         try:
@@ -74,7 +91,7 @@ def main():
             if voice_input!=None:
                 print(str(voice_input))
                 if str(voice_input).lower().find(AI_NAME.lower())!=-1 or in_convo==True:
-                    if command_handler(str(voice_input)):
+                    if c.process(voice_input):
                         in_convo=True
                         in_convo_time=datetime.now()
                         print('In conversation with the bot')
@@ -107,7 +124,16 @@ def main():
         except (KeyboardInterrupt, EOFError, SystemExit):
             break
 
-def command_handler(user_input):
-    pass
+
+
+
+class command_handler():
+    def __init__(self):
+        print('Command Handler - Booting')
+        self.commands={}
+        #load commands from commands.json
+    def process(self, user_input):
+        
+        print('Command Handler')
 
 main()
